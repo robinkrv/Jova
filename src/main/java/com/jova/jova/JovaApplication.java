ea95d7297b103489f86fa58dd8c957d4071c6cd6
robinkervella <121940567+robinkervella@users.noreply.github.com>
2024-04-08 18:26:34 +0200

    Initial commit

---
+++ src/main/java/com/jova/jova/JovaApplication.java
@@ -0,0 +1,13 @@
+package com.jova.jova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jova/jova/JovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jova.jova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
