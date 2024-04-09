a4bde03ec0ecd341b066cbf557357ade1b84dd6f
robinkervella <121940567+robinkervella@users.noreply.github.com>
2024-04-09 16:55:17 +0200

    Creation pages html

--- src/main/java/com/jova/jova/JovaApplication.java
+++ src/main/java/com/jova/jova/JovaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JovaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JovaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JovaApplication.class, args);
+    }
 
 }
--- src/test/java/com/jova/jova/JovaApplicationTests.java
+++ src/test/java/com/jova/jova/JovaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JovaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
